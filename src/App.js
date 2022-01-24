a340792fe0c145d31f0692052f6f02041da689ac
Aurélien <aurelien.strimelle@bstorm.be>
2022-01-24 16:11:36 +0100

    Liste de membre d'une ecole

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import './App.css';
 import FruitList from './components/fruit-list/fruit-list';
 import Person from './components/person/person';
+import SchoolMemberList from './components/school-member-list/school-member-list';
 
 function App() {
+
+  const people = [
+    { id: 1, firstname: 'Balthazar', lastname: 'Picsou', status: 'Prof'},
+    { id: 2, firstname: 'Riri', lastname: 'Duck', status: 'Eleve'},
+    { id: 3, firstname: 'Donald', lastname: 'Duck', status: 'Prof'},
+    { id: 4, firstname: 'Lili', lastname: 'Duck', status: 'Eleve'},
+    { id: 5, firstname: 'Zaza', lastname: 'Vanderquack', status: 'Eleve'},
+    { id: 6, firstname: 'Gontran', lastname: 'Bonheur', status: 'Prof'},
+    { id: 7, firstname: 'Fifi', lastname: 'Duck', status: 'Eleve'}
+  ];
+
   return (
     <div className="App">
       <h1>Demo 02</h1>
 
-      <FruitList />
-
-      <hr/>
-      
       <Person firstname='Zaza' lastname='Vanderquack' gender='F' />
       <Person firstname='Donald' lastname='Duck' gender='M' />
 
+      <hr />
+      <FruitList />
+
+      <hr/>
+      <SchoolMemberList members={people} />
     </div>
   );
 }
---
+++ src/components/school-member-list/school-member-list.jsx
@@ -0,0 +1,39 @@
+const SchoolMemberListItem = (props) => {
+    const {firstname, lastname, status} = props;
+
+    return (
+        <article>
+            <p>
+                {(status !== 'Prof') ? firstname : 'Professeur'} {lastname}
+                <br/>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, nostrum reiciendis dolore laudantium facere explicabo dolorem ullam aperiam nisi. Eum eveniet est doloremque incidunt veritatis consequatur dolore ullam animi consectetur.
+            </p>
+        </article>
+    )
+};
+
+const SchoolMemberList = (props) => {
+
+    const membersJSX = props.members.map(
+        member => <SchoolMemberListItem key={member.id} {...member} />
+    );
+
+    // const membersJSX = props.members.map(
+    //     member => <SchoolMemberListItem key={member.id} firstname={member.firstname}
+    //                                                     lastname={member.lastname}
+    //                                                     status={member.status} />
+    // );
+
+    return (<>
+        <h3>Listes des members d'une ecole</h3>
+        <section>
+            {membersJSX}
+        </section>
+    </>);
+};
+
+SchoolMemberList.defaultProps = {
+    members: []
+};
+
+export default SchoolMemberList;
